7f4073395b6bec4cef84733b05572119a2a20c4e
Brian Lego <legobw@gmail.com>
2020-06-23 13:06:04 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,9 @@
 def find_max_value(array)
-  # Add your solution here
+  counter=0 
+  max_value=nil 
+  while counter < array.length.times do
+    if array[counter]>array[counter-1]
+      array[counter]=max_value
+    counter += 1 
+  end   
 end
